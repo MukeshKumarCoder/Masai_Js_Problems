b219f8b6eb7e968ddc98d0ace0fc4b0ee74befb6
magupta1 <mg328790@gmail.com>
2023-09-27 20:14:56 +0530

    added new code

---
+++ Virtual McDonald's/index.js
@@ -0,0 +1,39 @@
+let burgur = document.getElementById("burgur");
+let pizza = document.getElementById("pizza");
+let sandwich = document.getElementById("sandwich");
+let orderBtn = document.getElementById("btn");
+let burgurimg = document.getElementById("burgurimg");
+let pizzaimg = document.getElementById("pizzaimg");
+let sandwichimg = document.getElementById("sandwichimg");
+let image = document.getElementById("image");
+let randomId = document.getElementById("randomId");
+let h1 = document.getElementById("h1");
+
+orderBtn.addEventListener("click", () => {
+  if (item.length == 0) {
+    alert("please select Atleast one food");
+  }
+  else{
+    
+  }
+  const myPromise = new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, randomTime(5, 10) * 500);
+  }).then(() => {
+    
+
+
+    h1.innerText = `Order id : ${randomOrderId()}`
+  });
+});
+
+function randomTime(min, max){
+     return Math.floor(Math.random() * (max - min) + 1); 
+};
+
+function randomOrderId(){
+    return Math.floor(Math.random() * 1000 + 1)
+};
+
+
